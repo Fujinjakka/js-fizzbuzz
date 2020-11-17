e3bf65d911c0fb90dd05920bf5add8a4120667c0
Fujinjakka <calvieri.marco@gmail.com>
2020-11-17 15:25:04 +0100

    ora funziona

--- script.js
+++ script.js
@@ -7,20 +7,22 @@
 
 
 
-var divisibileX3 = (i % 3);
-
-var divisibileX5 = (i % 5);
 
 for (var i = 1; i < 101; i++) {
-  document.getElementById("testo").innerHTML += i + "<br>";
-  console.log(i);
-  if (divisibileX3 == 0) {
+
+  var divisibileX3 = (i % 3);
+
+  var divisibileX5 = (i % 5);
+
+  if (divisibileX3 == 0 && divisibileX5 == 0) {
+    document.getElementById("testo").innerHTML += "FizzBuzz" + "<br>";
+  } else if (divisibileX3 == 0) {
     document.getElementById("testo").innerHTML += "Fizz" + "<br>";
   } else if (divisibileX5 == 0) {
     console.log("buzz");
     document.getElementById("testo").innerHTML += "Buzz" + "<br>";
-  } else if (divisibileX3 == 0 && divisibileX5 == 0) {
-    console.log("fizzBuzz");
-    document.getElementById("testo").innerHTML += "FizzBuzz" + "<br>";
+  } else {
+    document.getElementById("testo").innerHTML += i + "<br>";
+    console.log(i);
   }
 }
